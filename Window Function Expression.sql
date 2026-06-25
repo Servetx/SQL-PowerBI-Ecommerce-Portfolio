b3ced3207cc8351578b43953fe715220858d9fa0
Servet_SQL_Developer <jackwp694@gmail.com>
2026-06-25 08:24:23 +0300

    Refactor SQL joins for consistency and clarity

--- Window Function Expression.sql
+++ Window Function Expression.sql
@@ -20,10 +20,10 @@ SELECT
         PARTITION BY C.Customer_City 
         ORDER BY SUM(Oi.Price) DESC
     ) AS Sehir_ici_sira
-FROM df_Customers AS C
-JOIN df_Orders AS O 
+FROM df_Customers C
+JOIN df_Orders O 
     ON C.customer_id = O.customer_id
-JOIN df_OrderItems AS Oi 
+JOIN df_OrderItems Oi 
     ON O.order_id = Oi.order_id
 GROUP BY 
     C.customer_id, 
@@ -50,10 +50,10 @@ SELECT
         PARTITION BY C.Customer_City 
         ORDER BY SUM(Oi.Price) DESC
     ) AS Sehir_ici_sira
-FROM df_Customers AS C
-LEFT JOIN df_Orders AS O 
+FROM df_Customers C
+LEFT JOIN df_Orders O 
     ON C.customer_id = O.customer_id
-LEFT JOIN df_OrderItems AS Oi 
+LEFT JOIN df_OrderItems Oi 
     ON O.order_id = Oi.order_id
 WHERE 
     C.customer_city IN ('osasco', 'sao paulo', 'sorocaba')
@@ -83,10 +83,10 @@ SELECT
         PARTITION BY C.Customer_City 
         ORDER BY SUM(Oi.Price) DESC
     ) AS Sehir_ici_sira
-FROM df_Customers AS C
-LEFT JOIN df_Orders AS O 
+FROM df_Customers C
+LEFT JOIN df_Orders O 
     ON C.customer_id = O.customer_id
-LEFT JOIN df_OrderItems AS Oi 
+LEFT JOIN df_OrderItems Oi 
     ON O.order_id = Oi.order_id
 WHERE 
     C.customer_city IN ('osasco', 'sao paulo', 'sorocaba')
